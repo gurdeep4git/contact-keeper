82afb7176fc0fe08dcd8eab276c88e43b2e80351
Gurdeep <gurdeep.singh@quovantis.com>
2020-02-17 14:15:59 +0530

    Database connection

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require("express");
+const connectDB = require("./config/db");
 const app = express();
 
+// Connect Database
+connectDB();
 
 //Define routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
-app.get('/', (req, res) => res.json({ msg: "welcome to API..." }))
+//app.get('/', (req, res) => res.json({ msg: "welcome to API..." }))
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on ${PORT}`));
